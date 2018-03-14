058aeed9f57db4fa94bb05c9d7effb05fb96836f
builder-247 <olaakkonen@gmail.com>
2018-03-14 22:19:29 +0200

    Added randomVersion() and getRndInteger()

--- util/Utility.js
+++ util/Utility.js
@@ -1,9 +1,37 @@
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
-// Returns a number between 1 and 10.
-function randomDelay() {
-    return Math.floor(Math.random() * 10) + 1;
+function randomVersion() {
+    const supportedVersions = [
+        "1.8.0",
+        "1.8.1",
+        "1.8.2",
+        "1.8.3",
+        "1.8.4",
+        "1.8.5",
+        "1.8.6",
+        "1.8.7",
+        "1.8.8",
+        "1.8.9",
+        "1.9.0",
+        "1.9.1",
+        "1.9.2",
+        "1.9.3",
+        "1.9.4",
+        "1.10.0",
+        "1.10.1",
+        "1.10.2",
+        "1.11.0",
+        "1.11.1",
+        "1.11.2",
+        "1.12.0",
+        "1.12.1",
+    ];
+    return supportedVersions[getRndInteger(0, supportedVersions.length)]
 }
 
 module.exports = {
-    randomDelay
+    randomDelay,
+    randomVersion
 };
