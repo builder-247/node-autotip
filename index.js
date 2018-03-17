1b473832413b0734a9d314d9d039847a3456597c
FalseHonesty <skipboman0@gmail.com>
2018-03-17 19:03:27 -0400

    Finished up autotip protcol interaction

--- index.js
+++ index.js
@@ -31,13 +31,18 @@ function tipAll() {
     logger.info("Running /tipall...");
     bot.chat(`/tipall`);
 }
+
 let uuid;
+let autotipSession;
+
 bot.on(`login`, () => {
     logger.info(`Logged on ${options.host}:${options.port}`);
     setTimeout(() => {
         const session = bot._client.session;
         uuid = getUUID(bot);
-        login.login(uuid, session);
+        login.login(uuid, session, (aSession) => {
+            autotipSession = aSession;
+        });
         tipAll();
     }, 1000);
     setInterval(function () {
