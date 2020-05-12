04dc92b8c039dac03c2d491bc1edb3bcc08e5d8a
builder_247 <olaakkonen@gmail.com>
2020-05-13 01:28:06 +0300

    Fixed reconnecting not working, closes #14
    * This was caused by event listeners never being re-attached to bot instance

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const tipper = require('./lib/tipper');
 const util = require('./util/utility');
 const credentials = require('./credentials.json');
 
+let bot;
+let uuid;
+let autotipSession;
+
 const options = {
   host: 'mc.hypixel.net',
   port: 25565,
@@ -15,17 +19,6 @@ const options = {
   username: credentials.username,
   password: credentials.password,
 };
-let bot;
-(function init() {
-  bot = mineflayer.createBot(options);
-  bot._client.once('session', session => options.session = session);
-  bot.once('end', () => {
-    setTimeout(() => {
-      logger.info('Reconnecting...');
-      init();
-    }, 60000);
-  });
-}());
 
 function getUUID() {
   return bot._client.session.selectedProfile.id;
@@ -86,10 +79,7 @@ function chatLogger(message) {
   logger.game(ansi);
 }
 
-let uuid;
-let autotipSession;
-
-bot.on('login', () => {
+function onLogin() {
   uuid = getUUID(bot);
   logger.debug(`Logged on ${options.host}:${options.port}`);
   getLifetimeStats(uuid, (stats) => {
@@ -103,9 +93,9 @@ bot.on('login', () => {
       tipper.initTipper(bot, autotipSession);
     });
   }, 1000);
-});
+}
 
-bot.on('message', (message) => {
+function onMessage(message) {
   const msg = message.toString();
   chatLogger(message);
   if (msg.startsWith('You tipped')) {
@@ -130,11 +120,18 @@ bot.on('message', (message) => {
     || msg.startsWith('You\'ve already tipped that person today')) {
     tipper.tipFailed();
   }
-});
+}
 
-bot.on('kicked', (reason) => {
-  logger.info(`Kicked for ${reason}`);
-});
+(function init() {
+  bot = mineflayer.createBot(options);
+  bot._client.once('session', session => options.session = session);
+  bot.on('login', onLogin);
+  bot.on('message', onMessage);
+  bot.on('kicked', (reason) => {
+    logger.info(`Kicked for ${reason}`);
+  });
+  bot.once('end', () => setTimeout(init, 10000));
+}());
 
 function gracefulShutdown() {
   logger.info('Received kill signal, shutting down gracefully.');
